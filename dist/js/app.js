f71c00687bcc95bc2bfbf3f1c259764b4ef4147f
tadkim <icanhear@dgu.edu>
2016-04-09 16:23:52 +0900

    1. Go to Top btn 기능 추가 - add html markup : '.go-to' 클래스 - add css property : '.go-to' 속성 및 hover 속성 - add js function : jquery 기반 event

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,16 +4,19 @@
 
 $(function(){
 
-    $('.menu > ul > li').on("click", function(listEle){
+    $('.menu > ul > li').on("click", function(){
 
-        var $this = this;
-        var src_root = $(this).attr('class').split("_")[0];
-        var src_content = $(this).attr('class').split("_")[1];
-        var toSrc = 'http://yumm.co.kr/blogs/asset/contents/' + src_root + '/' + src_content + '/';
+        //클릭한 메뉴의 class에서 카테고리와 콘텐츠 타이틀을 얻음.
+        var $thisEl = $(this).attr('class').split("_");
+        // var src_root = $(this).attr('class').split("_")[0];
+        // var src_content = $(this).attr('class').split("_")[1];
+        var src_root = $thisEl[0]; // 대분류(카테고리)
+        var src_content = $thisEl[1]; //소분류(콘텐츠명)
+        var toSrc = 'http://yumm.co.kr/blogs/asset/contents/' + src_root + '/' + src_content + '/'; //이동할 url 구성
 
-
-        // $('#content').load(toSrc + ' #content', function() {
+        //page 내 .content 영역에 해당 html을 로드(with animation)
         $('.content').load(toSrc , function() {
+            //callback : 로드를 완료하면 해당 코드에서 pre, code 태그를 찾아 highlightjs 라이브러리 를 적용.
             $('pre code').each(function(i, block) {
                 hljs.highlightBlock(block);
             });
@@ -26,7 +29,32 @@ $(function(){
 });
 
 
+
+$(function(){
+
+    /* Go to top Btn  by tadkim */
+    // Show or hide the sticky footer button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.go-top').fadeIn(200);
+        } else {
+            $('.go-top').fadeOut(200);
+        }
+    });
+
+    // Animate the scroll to top
+    $('.go-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
+
+
+});
+
+
 $(document).ready(function(){
+
+    /* Menu Animation  by tadkim */
     // menu 클래스 바로 하위에 있는 a 태그를 클릭했을때
     $(".menu>a").click(function(){
         var submenu = $(this).next("ul");
@@ -38,4 +66,7 @@ $(document).ready(function(){
             submenu.slideDown();
         }
     });
+
+
 });
+
